77d427950d2bf3a932537c8901ce17f809c45c06
LuqueFlorencia <luque.e.florencia@gmail.com>
2022-12-06 23:38:28 -0300

    actualizacion 06/12

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 #URL LOGIN
 from django.contrib.auth import views as auth
@@ -34,4 +36,4 @@ urlpatterns = [
     #URL DE APLICACION
     path ('Noticias/', include('apps.noticias.urls')),
     path ('Usuario/', include('apps.usuarios.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
